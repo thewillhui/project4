b19f583aa725383c5eeb2a312657c66e3d3ef401
Lentz <lentz03@live.com>
2016-08-09 10:48:14 +0800

    morning commit

--- www/js/agent-controllers/agentListings-ctrl.js
+++ www/js/agent-controllers/agentListings-ctrl.js
@@ -1,3 +1,16 @@
-app.controller('ListingsCtrl', function(currentUser, $scope, $state){
-  function()
-}
+app.controller('ListingsCtrl',['$scope', '$http', '$state', 'SERVER', function($scope, $http, $state, SERVER){
+
+
+  $http({
+    method: 'GET',
+    url: SERVER.url + '/api/apartments',
+  }).then(function successCallback(response) {
+    console.log(response)
+      // this callback will be called asynchronously
+      // when the response is available
+    }, function errorCallback(response) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+    });
+
+}])
