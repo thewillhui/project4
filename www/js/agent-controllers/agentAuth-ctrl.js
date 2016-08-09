c17a5f8a38482e53edaf02772ae51e6b431335dc
Lentz <lentz03@live.com>
2016-08-09 19:29:52 +0800

    fixed agent signup

--- www/js/agent-controllers/agentAuth-ctrl.js
+++ www/js/agent-controllers/agentAuth-ctrl.js
@@ -1,18 +1,22 @@
 app.controller('AgentAuthCtrl', function(currentUser, $scope, $auth, $state) {
+  $scope.registrationForm = {areas: []};
+  $scope.loginForm = {};
 
   $scope.registrationForm = {};
   $scope.loginForm = {};
 
 
   $scope.handleRegBtnClick = function() {
-
     $auth.submitRegistration({
       mobile_number: $scope.registrationForm.mobile_number,
       password: $scope.registrationForm.password,
-      password_confirmation: $scope.registrationForm.password_confirmation
+      password_confirmation: $scope.registrationForm.password_confirmation,
+      areas: $scope.registrationForm.areas
+
     }, {
       config: 'agent'
     }).then(function(resp) {
+      console.log(resp)
         currentUser.setProperty(resp);
         console.log('this is from factory:');
         console.log(currentUser.getProperty());
@@ -51,6 +55,7 @@ app.controller('AgentAuthCtrl', function(currentUser, $scope, $auth, $state) {
         // handle error response
       });
   }
+
   $scope.region = {
     'Hong Kong Island': ['Aberdeen', 'Admiralty', 'Wan Chai', 'Tin Hau', 'Tai Hang', 'Tai Koo', 'Shau Kei Wan', 'Heng Fa Chuen', 'Sai Wan Ho', 'Quarry Bay', 'North Point', 'Fortress Hill', 'Mid-Levels', 'Island West', 'Island South', 'Chai Wan', 'Shek O', 'Central', 'Sheung Wan', 'Causeway Bay'],
     'Kowloon': ['Yau Tong', 'Lam Tin', 'Tsim Sha Tsui', 'Jordon', 'To Kwa Wan', 'Kowloon City', 'Tai Kok Tsui', 'Olympic', 'Kowloon Station', 'Sham Shui Po', 'Shek Kip Mei', 'San Po Kong', 'Wong Tai Sin', 'Prince Edward', 'Mong Kok', 'Yau Ma Tei', 'Lai Chi Kok', 'Cheung Sha Wan', 'Mei Foo', 'Lai King', 'Kwun Tong', 'Ngau Tau Kok', 'Kowloon Tong', 'Ho Man Tin', 'Yau Yat Tsuen', 'Kowloon Bay', 'Ngau Chi Wan', 'Hung Hom', 'Whampoa', 'Diamond Hill', 'Lok Fu'],
@@ -69,14 +74,14 @@ app.controller('AgentAuthCtrl', function(currentUser, $scope, $auth, $state) {
     return $scope.shownGroup === regionName;
   };
   //if an area is selected the function checks if it's in the enquiry object, if it is then remove it if not then add it. mimicks the checkbox functionality
-  // $scope.addAreaKey = function(area){
-  //   var areaArr = $scope.enquiry.areas;
-  //   var areaIndex = areaArr.indexOf(area);
-  //   if (areaIndex>=0){
-  //     areaArr.splice(areaIndex,1);
-  //   } else {
-  //     areaArr.push(area);
-  //   }
-  // };
+  $scope.addAreaKey = function(area){
+    var areaArr = $scope.registrationForm.areas;
+    var areaIndex = areaArr.indexOf(area);
+    if (areaIndex>=0){
+      areaArr.splice(areaIndex,1);
+    } else {
+      areaArr.push(area);
+    }
+  };
 })
 
