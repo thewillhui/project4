2b9b99b442c341710a3cbdfb8fc48ac84fee07a7
Fiona <fionacheung224@gmail.com>
2016-08-09 19:42:15 +0800

    renter appointments page

--- www/js/renter-controllers/renterAppointments-ctrl.js
+++ www/js/renter-controllers/renterAppointments-ctrl.js
@@ -1,7 +1,42 @@
 angular.module('simplyHome.controllers')
-.controller('RenterAppointmentsCtrl', function($http, $scope, SERVER){
+.controller('RenterAppointmentsCtrl', function($http, $scope, SERVER, $ionicModal){
   $scope.appointments = [];
+  $scope.renter_ratings = {
+    rr_overall_start: '',
+    rr_comment: '',
+    no_show: false,
+    spy: false
+  }
 
+   // Rating Modal
+  $ionicModal.fromTemplateUrl('templates/tabs-agent/rating-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal){
+    $scope.ratingModal = modal;
+  })
+  // $scope.propertyOpenModal = function(){
+  //   $scope.propertyModal.show();
+  // }
+
+  // Rate history Modal
+  $ionicModal.fromTemplateUrl('templates/tabs-agent/rate-history-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal){
+    $scope.rateHistModal = modal;
+  })
+
+  $scope.sendRating = function(){
+
+  }
+
+  $scope.enterRating = function(key, akey){
+    $scope.ratingModal.show();
+  }
+  $scope.showRating = function(key, akey){
+    $scope.rateHistModal.show();
+  }
   var getAppointments = function(){
     $http
       .get(SERVER.url + '/api/appointments')
